ac7a81c32f9467cc41d13aaf73609c4899b5158d
Narendra Maurya <nandymandy09@hotmail.com>
2020-04-20 14:27:50 +0530

    Addresses removed

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,10 +1,10 @@
-export const serverurl2 = "http://78.47.72.64:8001";
-export const baseURL = "http://78.46.185.253:8000/";
-export const serverurl = "http://78.46.185.253:8000";
-export const testnetURL = "https://ropsten.etherscan.io/tx/";
-export const aceAddress = "0xCb40609fAca34da6d72f0571CecAE155f3e6FE39";
-export const walletSocketAddress = "0x7e43Dd5d33e522Ba5622b08C95954ccb04a7284d";
+export const serverurl2 = "";
+export const baseURL = "";
+export const serverurl = "";
+export const testnetURL = "";
+export const aceAddress = "";
+export const walletSocketAddress = "";
 export const zkAssestHandlerAddress =
-  "0xC3c23d821A5e9C0cf891F92c9E221c98C07fbE6e";
+  "";
 export const testnetURI =
-  "https://ropsten.infura.io/v3/b368f72affd24424b558cac66853289d";
+  "";
